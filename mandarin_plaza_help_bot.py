2039cb2deb43f375f4decea565e3a22331d0e0cc
simonpaolo <simonelopez@hotmail.it>
2022-05-08 12:13:41 +0200

    init(bot), fix(all)

--- mandarin_plaza_help_bot.py
+++ mandarin_plaza_help_bot.py
@@ -1,96 +1,107 @@
 import json
+from aiogram import Bot, Dispatcher,executor,types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from tkinter import PhotoImage
-from telegram import *
-import telebot
-from telebot import *
-import wget
+
+
+token ='5396262849:AAHfchJgx8sQHSlDWRAX7K82IWjoAPJBvXA'
+bot=Bot(token=token)
+dp=Dispatcher(bot)
+main_content = json.load(open(r".\main_content.json"), encoding='utf-8')['contents']
+lang_buttons_list=[]
+lang_list=[]
+home_languaged=None
+##################indexing content###########################
+cont=0
+id_home_list=[]
+id_video_list=[]
+for i in main_content:
+   for j in i['home']:
+       j['id'] = cont
+       id_home_list.append(cont)
+       cont += 1
+       if(j['msg_type']=="video_list"):
+           for k in j['button_list']:
+               k['id']=cont
+               id_video_list.append(cont)
+               cont+=1
+
+#############################################################
+
+def dispatcher(id=-1):
+    for i in main_content:
+        for j in i['home']:
+            if(j['id']==id):
+                return j
+            elif(j['msg_type']=="video_list"):
+                for k in j['button_list']:
+                    if(k['id']==id):
+                        return k
 
 
 
-token ='1629663298:AAGIgrDZlmIyb-ORnlLm7uOiU9uYx4uGWZI'
-bot = telebot.TeleBot(token)
-main_content=json.load(open(r"C:\Users\Asus\Desktop\Mandarin-plaza-help-bot\main_content.json"))
-naz=0
 
+for i in main_content:
+    lang=i['lang']
+    lang_list.append(lang)
+    lang_buttons_list.append(InlineKeyboardButton(text=lang,callback_data=lang))
+keyboard_inline = InlineKeyboardMarkup().add(*lang_buttons_list)
 
-def ling():
-   lingue=[]
-   for c in main_content['contents']:
-         if c['lang'] not in lingue:
-             lingue.append(c['lang'])
-   return lingue
-   
+def home_keyboard(content):
+    home_button_list=[]
+    for i in content:
+        home_button_list.append(InlineKeyboardButton(text=i['label'],callback_data=i['id']))
+    return InlineKeyboardMarkup().add(*home_button_list)
+def videos_keyboard(content):
+    videos_button_list=[]
+    for i in content["button_list"]:
+        videos_button_list.append(InlineKeyboardButton(text=i['label'],callback_data=i['id']))
+    return InlineKeyboardMarkup().add(*videos_button_list)
 
-
-
-def creaMark(row,s,n):
-    print("naz:",n)
-    naz=n
-    l=main_content['contents'][naz][s]
-    ret=types.InlineKeyboardMarkup(row_width=row)
-    lang=types.InlineKeyboardButton("change language",callback_data="lang")
-    ret.add(lang)
-    
-    for i in l :
-         btn=types.InlineKeyboardButton(i['label'],callback_data=i['text'])
-         ret.add(btn)
-         
-    return ret
-
-@bot.callback_query_handler(func= lambda  call:True)
-def video_callback(call): 
-    lingue=ling()
-    id=call.from_user.id
-    data=call.data
-    query_id=call.id
-
-    if(data in lingue):
-        n=lingue.index(data)
-        print(n)
-        m=creaMark(1,'home',n)
-        bot.answer_callback_query(query_id)
-        bot.send_message(id,data,reply_markup=m)
-
-
-        
-
-    elif(data=="lang"):
-        m=types.InlineKeyboardMarkup(row_width=1)
-        for i in lingue:
-             btn=types.InlineKeyboardButton(i,callback_data=i)
-             m.add(btn)
-        bot.send_message(id,"language:",reply_markup=m)
-        
-    elif(data =="tutorial"):
-         m=types.InlineKeyboardMarkup(row_width=1)
-         
-         l=main_content['contents'][naz]['home'][2]['button_list']
-         for i in l:
-             btn=types.InlineKeyboardButton(i['label'],callback_data=i['src'])
-             m.add(btn)
-         bot.send_message(id,"tutorial:",reply_markup=m)
-         bot.answer_callback_query(query_id)
-
-    elif("mp4" in data):
-         bot.send_video(id,video=open(data,'rb'))
-         bot.answer_callback_query(query_id)
-
-    else:
-        
-        bot.send_message(id,data)
-        
-        bot.answer_callback_query(query_id)
-    return
+@dp.message_handler(commands=['start','help'])
+async def welcome(message: types.Message):
+    await message.reply("hello",reply_markup=keyboard_inline)
 
 
 
-def main():
-    naz=0
-    markup=creaMark(1,'home',naz)
-    while(True):
-        @bot.message_handler(commands=['help','start'] )
-        def send_welcome(msg):
-            id=msg.chat.id
-            bot.send_message(id,"ciao",reply_markup=markup)
-        bot.polling(none_stop=True)
-main()
+@dp.message_handler(commands=['home'])
+async def welcome(message: types.Message):
+    await message.reply(home_languaged)
+
+
+
+
+@dp.callback_query_handler(text=lang_list)
+async def select_lang(call: types.CallbackQuery):
+    for c in main_content:
+        if c['lang'] == call.data:
+           home_languaged=c['home']
+           await call.message.reply(text="Home",reply_markup=home_keyboard(home_languaged))
+    await call.answer()
+
+
+@dp.callback_query_handler(text=id_home_list)
+async def get_content(call: types.CallbackQuery):
+    id=int(call.data)
+    content=dispatcher(id)
+
+    if(content["msg_type"]=="text"):
+        #TODO inserire nella risposta foto se contenute
+        await call.message.reply(text=content['text'])
+
+
+    elif(content["msg_type"]=="video_list"):
+        await call.message.reply(text="videos",reply_markup=videos_keyboard(content))
+
+    await call.answer()
+
+@dp.callback_query_handler(text=id_video_list)
+async def get_videos(call: types.CallbackQuery):
+    id = int(call.data)
+    content = dispatcher(id)
+    #TODO inviare video
+    await call.message.reply(text="ecco il video: "+content['label'])
+    await call.answer()
+
+
+executor.start_polling(dp)
